0d11743d8a71c29763e8df89d184e586928169e2
ErdalErdem <erda0028@stud.kea.dk>
2023-02-09 14:36:39 +0100

    Initial commit

---
+++ src/main/java/com/example/erdalshjemmeside/ErdalsHjemmesideApplication.java
@@ -0,0 +1,13 @@
+package com.example.erdalshjemmeside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErdalsHjemmesideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErdalsHjemmesideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/erdalshjemmeside/ErdalsHjemmesideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erdalshjemmeside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErdalsHjemmesideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
