831906f55d5c730dc2eccc6f8e2d9df2279b1e44
ErdalErdem <erda0028@stud.kea.dk>
2023-02-09 14:39:43 +0100

    Portfolio

--- src/main/java/com/example/erdalshjemmeside/ErdalsHjemmesideApplication.java
+++ src/main/java/com/example/erdalshjemmeside/ErdalsHjemmesideApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ErdalsHjemmesideApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ErdalsHjemmesideApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ErdalsHjemmesideApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/erdalshjemmeside/ErdalsHjemmesideApplicationTests.java
+++ src/test/java/com/example/erdalshjemmeside/ErdalsHjemmesideApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ErdalsHjemmesideApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
